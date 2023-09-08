e5540dd9b9114a6aba1b1a6f3fdf5436fbe1e3e4
SidVigorous <143992470+SidVigorous@users.noreply.github.com>
2023-09-08 18:36:00 +0530

    changes

--- src/Components/Virtual/Virtual.js
+++ src/Components/Virtual/Virtual.js
@@ -1,19 +1,16 @@
 import React from "react";
 import "./Virtual.css";
-
 import Card from "react-bootstrap/Card";
 import baccarat from "../../Assets/Images/Baccarat-_Virtual_.png";
 import poker from "../../Assets/Images/Poker-20-20-_Virtual_.png";
 import race from "../../Assets/Images/Race-20-20-_Virtual_.png";
 import amar from "../../Assets/Images/Amar-Akbar-Anthony-_Virtual_.png";
 import theenpati from "../../Assets/Images/Teenpatti-Open-_virtual_.png";
-
 import muflis from "../../Assets/Images/Muflis Teenpatti (virtual).webp";
 import casino from "../../Assets/Images/Casino War (Virtual).webp";
 import teenpati from "../../Assets/Images/Teenpatti Test (virtual).webp";
 import trio from "../../Assets/Images/Trio (virtual).webp";
 import queen from "../../Assets/Images/Queen (virtual).webp";
-
 import t20 from "../../Assets/Images/Teenpatti T20 (Virtual).webp";
 import meter from "../../Assets/Images/Casino Meter (Virtual).webp";
 import andar from "../../Assets/Images/Andar Bahar (Virtual).webp";
